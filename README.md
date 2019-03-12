3f4b9b0a735680b527f57a0a2484a7b6c50a070d
Furqan Khan <furqankhaan@gmail.com>
2019-03-13 07:23:57 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# 1-Controlled-component
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="logo.svg" className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" />
+          <p className="echo">Echo:</p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
